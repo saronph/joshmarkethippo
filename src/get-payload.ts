40b66b32603ac0868e2400c26fec6a3c3d1e4af4
Saron <saron.ph@gmail.com>
2024-03-07 20:39:16 -0300

    added sign up page

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
+import payload from "payload";
 import { InitOptions } from "payload/config";
 
 dotenv.config({
@@ -19,4 +20,29 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: Args) => {};
+export const getPayloadClient = async ({ initOptions }: Args) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD_SECRET is missing");
+  }
+
+  if (cached.client) {
+    return cached.client;
+  }
+
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cached.client = await cached.promise;
+  } catch (e: unknown) {
+    cached.client = null;
+    throw e;
+  }
+
+  return cached.client;
+};
