c4b34b7860468875a12e446815ae0ae58ce85c21
Saron <saron.ph@gmail.com>
2024-03-07 20:50:28 -0300

    added trpc provider

---
+++ src/components/Providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
+import { QueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+const Providers = () => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() => trpc.createClient);
+};
+
+export default Providers;
